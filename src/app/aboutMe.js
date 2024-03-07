a7c21b9e6b4a0d536ba30bc482bcc3ebcf0859bd
Md Mahmudur Reza <mahmudreza27@gmail.com>
2024-03-07 16:19:46 -0500

    created header and about me section

---
+++ src/app/aboutMe.js
@@ -0,0 +1,16 @@
+import styles from './aboutMe.module.css'
+
+
+export default function AboutMe(){
+    return (
+        <div className={styles.aboutmeSection}>
+            <div className={styles.textSection}>
+text
+            </div>
+            <div className={styles.imageSection}>
+image
+            </div>
+
+            </div>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import Navbar from './navbar'
+import Header from './header'
+import AboutMe from './aboutMe'
 export default function Home() {
   return (
     <main className={styles.main}>
       <Navbar />
-
+      <Header />
+      <AboutMe />
 
       <div className={styles.socialMedia}>
 
