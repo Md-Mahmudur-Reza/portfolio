4654d3156d94dd4a9735bd841770e22d1440114a
Md Mahmudur Reza <mahmudreza27@gmail.com>
2024-04-16 17:40:37 -0400

    modified navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,7 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import Navbar from '../module/navbar/navbar'
-import Header from '../module/header/header'
-import AboutMe from '../module/aboutme/aboutMe'
+import styles from "./page.module.css";
+import Navbar from "../module/navbar/navbar";
+import Header from "../module/header/header";
+import AboutMe from "../module/aboutme/aboutMe";
 export default function Home() {
   return (
     <main className={styles.main}>
@@ -10,12 +9,8 @@ export default function Home() {
       <Header />
       <AboutMe />
 
-      <div className={styles.socialMedia}>
-
-      </div>
-      <div className={styles.body}>
-
-      </div>
+      <div className={styles.socialMedia}></div>
+      <div className={styles.body}></div>
     </main>
-  )
+  );
 }
--- src/module/navbar/navbar.js
+++ src/module/navbar/navbar.js
@@ -1,19 +1,61 @@
-import styles from './navbar.module.css'
+"use client";
 
+import React, { useState } from "react";
+import Image from "next/image";
 
-export default function Navbar(){
-    return (
-        <div className={styles.navbar}>
-        <div className={styles.logo}>
-          R
+import logo from "../../../public/only_logo_transperant_border_bg.png";
+
+import styles from "./navbar.module.css";
+
+export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.navbarContainer}>
+        <div className={styles.navbarLeft}>
+          <Image
+            // onClick={goHome}
+            className={styles.logo}
+            src={logo}
+            alt="logo"
+          />
         </div>
-        <ul className={styles.navItems}>
-          <li><a href='#'>01. About</a></li>
-          <li><a href='#'>02. Experience</a></li>
-          <li><a href='#'>03. Work</a></li>
-          <li><a href='#'>04. Contact</a></li>
-          <li><a href='#'>Resume</a></li>
-        </ul>
+        <div className={`${styles.navbarRight} `}>
+          <button
+            className={styles.navbarToggle}
+            onClick={toggleNavbar}
+            aria-expanded={isOpen ? "true" : "false"}
+            aria-controls="navbarLinks"
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+          <ul
+            className={`${styles.navbarLinks} ${isOpen ? styles.active : ""}`}
+            id="navbarLinks"
+          >
+            <li>
+              <a className={styles.navbarText}>About</a>
+            </li>
+            <li>
+              <a className={styles.navbarText}>Experience</a>
+            </li>
+            <li>
+              <a className={styles.navbarText}>Work</a>
+            </li>
+            <li>
+              <a className={styles.navbarText}>Contact</a>
+            </li>
+            <li>
+              <div className={styles.resumeButton}> Resume</div>
+            </li>
+          </ul>
+        </div>
       </div>
-    )
+    </nav>
+  );
 }
