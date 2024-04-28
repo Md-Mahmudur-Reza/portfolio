a84cc865ccec5c8af06d673d4fea9d66ee22745e
Md Mahmudur Reza <mahmudreza27@gmail.com>
2024-04-28 12:31:11 -0400

    modified resume

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,21 @@
+import Head from "next/head";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./globals.css";
 
 export const metadata = {
-  title: "Md Reza",
-  description: "Just a little bit of myself",
+  title: "Md Mahmudur Reza",
+  description: "Portfolio of Md Mahmudur Reza",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <body>{children}</body>
     </html>
   );
